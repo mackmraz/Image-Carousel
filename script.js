1a64bbd8784826d4a65c03d40e8b3824c9a25b06
Mack Mraz <mackmraz@gmail.com>
2022-11-16 19:37:03 -0500

    Updates the progress bar JS

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ function onHandleClick(handle) {
         slider.style.setProperty("--slider-index", sliderIndex + 1)
     }
 }
+
+if (handle.classList.container("right-handle")) {
+    slider.style.setProperty("--slider-index", sliderIndex - 1)
+    progressBar.children[sliderIndex].classList.remove("activate")
+    progressBar.children[sliderIndex - 1].classList.add("activate")
+}
+
+if (handle.classList.container("left-handle")) {
+    slider.style.setProperty("--slider-index", sliderIndex + 1)
+    progressBar.children[sliderIndex].classList.remove("activate")
+    progressBar.children[sliderIndex + 1].classList.add("activate")
+}
