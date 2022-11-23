f8ac7ca4bfffff9444f8f315d3f3f9bfa0c2eaba
mackmraz <60161943+mackmraz@users.noreply.github.com>
2022-11-23 12:55:05 -0500

    Fixes progress bar to work and dynamically change to amount of images shown.

--- script.js
+++ script.js
@@ -12,27 +12,47 @@ window.addEventListener("resize", (e) => {
     //recalculate progress bar
 })
 
+document.querySelectorAll(".progress-bar").forEach
+(calculateProgressBar)
+
+function calculateProgressBar(progressBar) {
+    progressBar.innerHTML = ""
+    const slider = progressBar.closest(".row").querySelector(".slider")
+    const itemCount = slider.children.length
+    const itemsPerScreen = parseInt(
+        getComputedStyle(slider).getPropertyValue("--items-per-screen")
+    )
+    const sliderIndex = parseInt(
+            getComputedStyle(slider).getPropertyValue("--slider-index")
+    )
+    const progressBarItemCount = Math.ceil
+    (itemCount / itemsPerScreen)
+    for (let i = 0; i < progressBarItemCount; i++) {
+        const barItem = document.createElement("div")
+        barItem.classList.add("progress-item")
+        if (i === sliderIndex) {
+            barItem.classList.add("active")
+        }
+        progressBar.append(barItem)
+    }
+}
+
 function onHandleClick(handle) {
+    const progressBar = handle.closest(".row").querySelector(".progress-bar")
     const slider = handle.closest(".container").querySelector(".slider")
     const sliderIndex = parseInt(
         getComputedStyle(slider).getPropertyValue("--slider-index"))
     if (handle.classList.contains("left-handle")) {
         slider.style.setProperty("--slider-index", sliderIndex - 1)
+        progressBar.children[sliderIndex].classList.remove("active")
+        progressBar.children[sliderIndex - 1].classList.add("active")
     }
-
     if (handle.classList.contains("right-handle")) {
         slider.style.setProperty("--slider-index", sliderIndex + 1)
+        progressBar.children[sliderIndex].classList.remove("active")
+        progressBar.children[sliderIndex + 1].classList.add("active")
     }
 }
 
-if (handle.classList.container("right-handle")) {
-    slider.style.setProperty("--slider-index", sliderIndex - 1)
-    progressBar.children[sliderIndex].classList.remove("activate")
-    progressBar.children[sliderIndex - 1].classList.add("activate")
-}
 
-if (handle.classList.container("left-handle")) {
-    slider.style.setProperty("--slider-index", sliderIndex + 1)
-    progressBar.children[sliderIndex].classList.remove("activate")
-    progressBar.children[sliderIndex + 1].classList.add("activate")
-}
+
